b44e3675ad0afbb56c7476803fc94b3776186d62
Syntox32 <syntox32@gmail.com>
2015-08-26 19:35:29 +0200

    Separated code into Haven.Core

---
+++ Haven.Core/CookieClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haven.Core
+{
+    /// <summary>
+    /// Class that inherits WebClient to be able to hold cookies
+    /// </summary>
+    [System.ComponentModel.DesignerCategory("Code")]
+    public class CookieClient : WebClient
+    {
+        private CookieContainer _cookie = new CookieContainer();
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest req = (HttpWebRequest)base.GetWebRequest(address);
+            req.ProtocolVersion = HttpVersion.Version10;
+
+            if (req is HttpWebRequest)
+                (req as HttpWebRequest).CookieContainer = _cookie;
+
+            return req;
+        }
+    }
+}
---
+++ Haven.Core/Wallpaper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haven.Core
+{
+    public enum Extension
+    {
+        Jpg,
+        Png
+    }
+
+    public enum Purity
+    {
+        SFW,
+        Sketchy,
+        NSFW
+    }
+
+    /// <summary>
+    /// Holds the information of a wallpaper.
+    /// </summary>
+    public class Wallpaper
+    {
+        private const string _downloadUrl = @"http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.{1}";
+
+        public Extension Extension { get; set; }
+        public int Id { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Purity Purity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Url
+        {
+            get { return String.Format(_downloadUrl, Id, Extension == Extension.Jpg ? "jpg" : "png"); }
+        }
+
+        public string Name
+        {
+            get { return String.Format("wallpaper-{0}", Id); }
+        }
+
+        public Wallpaper(int id)
+        {
+            this.Id = id;
+            this.Extension = Extension.Jpg;
+        }
+
+        public Wallpaper(int id, int width, int height)
+            : this(id)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
